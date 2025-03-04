444055e6d03b9734e01e4994a527a0c7e7a984ee
Mohand0sman <mohand.osman.adam@gmail.com>
2025-03-04 20:48:43 +0200

    add projects section

--- js/main.js
+++ js/main.js
@@ -21,3 +21,87 @@ function closeMenu() {
     navMedia.classList.remove('active');
     document.body.style.overflow = 'auto'; // إرجاع التمرير
 }
+
+
+// فلتر عرض التطبيقات او الويب
+function filterProjects(type, btn) {
+    let cards = document.querySelectorAll('.swaper-c');
+    let buttons = document.querySelectorAll('.tab-btn');
+    
+    buttons.forEach(button => button.classList.remove('activ'));
+    btn.classList.add('activ');
+
+    cards.forEach(card => {
+        card.style.display = card.classList.contains(type) ? 'block' : 'none';
+    });
+}
+
+// لتفعيل عرض مشاريع الويب كخيار افتراضي عند تحميل الصفحة
+document.addEventListener("DOMContentLoaded", function() {
+    filterProjects('web', document.querySelector('.tab-btn.activ'));
+});
+
+
+
+// السوابر الخاص بالصور 
+
+    // تفعيل Swiper لكل عنصر يحمل الكلاس mySwiper
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.mySwiper').forEach(function(swiperEl) {
+            new Swiper(swiperEl, {
+            slidesPerView: 1,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            // إذا أردت تفعيل Pagination داخل الكاردات
+            // pagination: {
+            //   el: swiperEl.querySelector('.swiper-pagination'),
+            //   clickable: true,
+            // },
+            });
+        });
+        });
+
+
+        // السواب الغير مستعمل
+// const swiperEl = document.querySelector('swiper-container');
+// const swiper = swiperEl.swiper;
+
+// var appendNumber = 4;
+// var prependNumber = 1;
+// document
+//   .querySelector(".prepend-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide([
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
+//     ]);
+//   });
+// document
+//   .querySelector(".prepend-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide(
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>"
+//     );
+//   });
+// document
+//   .querySelector(".append-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.appendSlide(
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>"
+//     );
+//   });
+// document
+//   .querySelector(".append-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.appendSlide([
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
+//     ]);
+//   });
